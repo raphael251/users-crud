ef22f6b01560037cc68d25b4c4e46495162ade1e
Raphael Passos <raphael251@hotmail.com>
2024-01-17 09:39:33 -0300

    chore: remove prepare statement in the user repository create method

--- internal/infra/database/user_repository.go
+++ internal/infra/database/user_repository.go
@@ -15,16 +15,11 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 	return &UserRepository{DB: db}
 }
 
-// TODO: remove prepare statement
 func (r *UserRepository) Create(user *user.User) error {
-	stmt, err := r.DB.Prepare("insert into users(id, name, birth_date, email, password, address) values(?, ?, ?, ?, ?, ?)")
-
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(user.ID, user.Name, user.BirthDate, user.Email, user.Password, user.Address)
+	_, err := r.DB.Exec(
+		"insert into users(id, name, birth_date, email, password, address) values(?, ?, ?, ?, ?, ?)",
+		user.ID, user.Name, user.BirthDate, user.Email, user.Password, user.Address,
+	)
 
 	if err != nil {
 		return err
