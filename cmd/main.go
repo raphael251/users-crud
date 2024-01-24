667732286db54b52a093f01ed3bcc9a7759b7911
Raphael Passos <raphael251@hotmail.com>
2024-01-23 22:59:59 -0300

    chore: bring handlers, routers and server to main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/raphael251/users-crud/configs"
-	"github.com/raphael251/users-crud/internal/infra/web/routers"
-	"github.com/raphael251/users-crud/internal/infra/web/server"
+	"github.com/raphael251/users-crud/internal/domain/usecase"
+	"github.com/raphael251/users-crud/internal/domain/utils"
+	"github.com/raphael251/users-crud/internal/infra/database"
+	"github.com/raphael251/users-crud/internal/infra/web/httputils"
 )
 
 func main() {
@@ -26,10 +33,139 @@ func main() {
 	}
 	defer db.Close()
 
-	server := server.NewServer(configs.ServerPort)
+	router := chi.NewRouter()
 
-	server.AddRouter("/api/v1/health", routers.GenerateApplicationRouter())
-	server.AddRouter("/api/v1/users", routers.GenerateUserRouter(db))
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
 
-	server.Start()
+	router.Route("/api/v1/health", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+		})
+	})
+
+	userRepository := database.NewUserRepository(db)
+
+	router.Route("/api/v1/users", func(r chi.Router) {
+		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+			var receivedUser usecase.CreateUserInputDTO
+
+			err := json.NewDecoder(r.Body).Decode(&receivedUser)
+
+			if err != nil {
+				httputils.RespondBadRequest(w, r, []string{"Invalid JSON. Please see the docs."})
+				return
+			}
+
+			if validationErrors := receivedUser.Validate(); validationErrors != nil {
+				errs := make([]string, 0)
+				for _, err := range validationErrors {
+					errs = append(errs, err.Error())
+				}
+
+				httputils.RespondBadRequest(w, r, errs)
+				return
+			}
+
+			createUserUseCase := usecase.NewCreateUserUseCase(userRepository)
+			result, err := createUserUseCase.Execute(receivedUser)
+
+			if err != nil {
+				if cerr, ok := err.(*utils.UseCaseError); ok {
+					if cerr.Type == utils.ValidationError {
+						httputils.RespondBadRequest(w, r, []string{cerr.Error()})
+						return
+					}
+
+					if cerr.Type == utils.BusinessRuleViolationError {
+						httputils.RespondBadRequest(w, r, []string{cerr.Error()})
+						return
+					}
+				}
+
+				log.Println(err.Error())
+				httputils.RespondInternalServerError(w, r)
+				return
+			}
+
+			contentLocation := fmt.Sprintf("/api/v1/users/%s", result.ID)
+			httputils.RespondCreated(w, r, contentLocation)
+		})
+
+		r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
+			receivedUserId := chi.URLParam(r, "id")
+
+			findOneUserUseCase := usecase.NewFindOneUserUseCase(userRepository)
+			user, err := findOneUserUseCase.Execute(receivedUserId)
+
+			if err != nil {
+				if cerr, ok := err.(*utils.UseCaseError); ok {
+					if cerr.Type == utils.ValidationError {
+						httputils.RespondBadRequest(w, r, []string{cerr.Error()})
+						return
+					}
+
+					if cerr.Type == utils.NotFoundError {
+						httputils.RespondNotFound(w, r)
+						return
+					}
+				}
+
+				log.Println(err.Error())
+				httputils.RespondInternalServerError(w, r)
+				return
+			}
+
+			httputils.RespondOK(w, r, user)
+		})
+
+		r.Put("/{id}", func(w http.ResponseWriter, r *http.Request) {
+			var receivedUser usecase.UpdateUserInputDTO
+
+			err := json.NewDecoder(r.Body).Decode(&receivedUser)
+
+			if err != nil {
+				httputils.RespondBadRequest(w, r, []string{"Invalid JSON. Please see the docs."})
+				return
+			}
+
+			if validationErrors := receivedUser.Validate(); validationErrors != nil {
+				errs := make([]string, 0)
+				for _, err := range validationErrors {
+					errs = append(errs, err.Error())
+				}
+
+				httputils.RespondBadRequest(w, r, errs)
+				return
+			}
+
+			receivedUser.ID = chi.URLParam(r, "id")
+
+			updateUserUseCase := usecase.NewUpdateUserUseCase(userRepository)
+			err = updateUserUseCase.Execute(receivedUser)
+
+			if err != nil {
+				if cerr, ok := err.(*utils.UseCaseError); ok {
+					if cerr.Type == utils.ValidationError {
+						httputils.RespondBadRequest(w, r, []string{cerr.Error()})
+						return
+					}
+
+					if cerr.Type == utils.BusinessRuleViolationError {
+						httputils.RespondBadRequest(w, r, []string{cerr.Error()})
+						return
+					}
+				}
+
+				log.Println(err.Error())
+				httputils.RespondInternalServerError(w, r)
+				return
+			}
+
+			httputils.RespondNoContent(w, r)
+		})
+	})
+
+	log.Printf("Server listening on port %s", configs.ServerPort)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", configs.ServerPort), router))
 }
